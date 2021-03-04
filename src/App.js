3b89949e081209fdcf7fef4eeb753be0df4f7c79
carloslavie <carloslavie@gmail.com>
2021-03-04 10:54:46 -0300

    modificacion fetch url

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
 
 
   const consultarAPI = async ()=>{
-    const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
     const frase = await api.json();
     guardarFrase(frase[0]);
   }
